2c792241ad6a4d95b7c511c1bd566ef85e99530e
jonathan.skeete <jonathan.skeete@cs-c09-28.mec.science>
2025-02-05 08:52:42 -0500

    added .gitignore and Week01

---
+++ Week01/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "argc: " << argc << std::endl;
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
+    }
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
