f47acb315b3ca04c1874a5910f63ab654bd181b3
jonathan-skeete <jonathan.skeete.dev@gmail.com>
2025-02-06 12:29:04 -0500

    update

---
+++ gfetch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+UPSTREAM_URL="https://github.com/Jonathan-Skeete/CS151_Spring2025.git"
+
+if git remote get-url upstream &>/dev/null; then
+    echo "Remote upstream already exists."
+else
+    echo "Adding upstream..."
+    git remote add upstream "$UPSTREAM_URL"
+fi
+
+git fetch upstream
+git merge upstream/main
