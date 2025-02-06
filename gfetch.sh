43781308b00a39cc9bc21c2cc72b35804edd15d1
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-02-06 16:07:53 -0500

    update

--- gfetch.sh
+++ gfetch.sh
@@ -2,12 +2,12 @@
 
 UPSTREAM_URL="https://github.com/Jonathan-Skeete/CS151_Spring2025.git"
 
-if git remote get-url upstream &>/dev/null; then
-    echo "Remote upstream already exists."
-else
+if ! git remote get-url upstream &>/dev/null; then
     echo "Adding upstream..."
     git remote add upstream "$UPSTREAM_URL"
 fi
 
 git fetch upstream
 git merge upstream/main
+
+# ./gfetch.sh
--- gpush.sh
+++ gpush.sh
@@ -4,3 +4,5 @@ git pull
 git add .
 git commit -m "update"
 git push
+
+# ./gpush.sh
