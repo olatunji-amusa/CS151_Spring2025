0790f4476c445e424e34ed22bcab6414271166e0
olatunji-amusa <tunjiamusa23@gmail.com>
2025-02-07 21:08:11 +0000

    Repository Update

---
+++ Week02/Lab/mygrade.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char letter = 'A';
+    double number = 96;
+    
+    cout << "letter grade is " <<
+    letter << endl;
+    
+
+    cout << "number grade is " << number;
+    
+}
