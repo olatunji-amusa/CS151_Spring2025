fa7a933140c65d651d3a7eca49666c1fd6b88a06
jonathan-skeete <jonathan.skeete.dev@gmail.com>
2025-02-14 15:00:02 -0500

    Repository Update - 2025-02-14 15:00:02

---
+++ Week03/week03.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    /*
+    string hello = "Hello, World!";
+    char letter = 'A';
+    cout << sizeof(letter) << endl;
+    cout << sizeof("A") << endl;
+    string letter_a = "A";
+    cout << sizeof(letter_a) << endl;
+    */
+
+    // cout << hello << endl;
+
+    bool check = 67;
+    cout << boolalpha << check << endl;
+
+    auto word = "test";
+    cout << word << endl;
+
+    const double PI = 3.1415; // This is pi
+    // PI = 3.14159;
+    cout << PI << endl;
+
+
+}
