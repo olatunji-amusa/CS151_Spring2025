81132d95b479c0cbb90bcdcff54cb63eb7d1ce58
jonathan-skeete <jonathan.skeete.dev@gmail.com>
2025-02-21 16:59:15 -0500

    Repository Update - 2025-02-21 16:59:15

---
+++ ExtraCredit/dog_years.py
@@ -0,0 +1,20 @@
+"""
+Write a program that prompts the user to enter their age.
+Display to the user their age in dog years.
+
+Type your code after the ellipsis (...) below.
+
+Run your program using the run button or
+type the following in your terminal:
+
+python ExtraCredit/dog_years.py
+
+or
+
+python dog_years.py
+
+"""
+
+if __name__ == "__main__":
+    ...
+    
