9c2c0cdf9a6ce48f3fc3f24f66b52435dc665cac
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-02-28 16:12:59 -0500

    Repository Update - 2025-02-28 16:12:59

---
+++ Week05/Lab/attack.cpp
@@ -0,0 +1,12 @@
+/*
+Write a program that stores a whole number value in a variable called attack,
+and a variable called critical that is set to false.
+
+For any attack there is a 20% chance of a critical hit.
+
+Declare a variable that holds a random number between 1 and 100.
+If the random number is between 1 and 20 inclusively, set the critical hit variable to true.
+
+If the critical hit variable is true, multiply the attack power by 2.
+Display the attack power and whether or not the attack was a critical hit.
+*/
