2a2ab1af0ed1877f59ac9bff789d158a0dda0bfe
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-02-28 14:17:25 -0500

    Repository Update - 2025-02-28 14:17:25

---
+++ ExtraCredit/logarithm.py
@@ -0,0 +1,13 @@
+"""
+Write a program that displays the log of 10000 to the base 10,
+and the log of 1024 to the base 2.
+
+Cannot use log2 or log10 functions.
+Only use the log function from the math module.
+
+"""
+from math import log
+
+if __name__ == "__main__":
+    ...
+    
