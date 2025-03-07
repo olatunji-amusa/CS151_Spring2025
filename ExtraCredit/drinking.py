feac640aabc83ecf897762312d1b9a5a50ad3e18
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-03-07 13:26:08 -0500

    Repository Update - 2025-03-07 13:26:07

---
+++ ExtraCredit/drinking.py
@@ -0,0 +1,11 @@
+"""
+Write a program that askes the user if they have their ID (yes or no).
+If the user has their ID, ask the user if they are 21 years or older (yes or no).
+If the user is 21 years or older, display to the user that they can drink.
+If the user is less than 21 years old, display to the user that they cannot drink.
+If the user does not have their ID, display to the user that they cannot drink.
+If the user does not input yes or no, display to the user they cannot drink.
+"""
+
+if __name__ == "__main__":
+    ...
