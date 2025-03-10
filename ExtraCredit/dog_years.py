c567fad444e43220c11617172c978f65083eec96
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-03-10 09:09:39 -0400

    Repository Update - 2025-03-10 09:09:38

--- ExtraCredit/dog_years.py
+++ ExtraCredit/dog_years.py
@@ -16,5 +16,6 @@ python dog_years.py
 """
 
 if __name__ == "__main__":
-    ...
-    
+    age: int = int(input("Enter your age: "))
+    dog_years: int = age * 7
+    print(f"Your age in dog years is: {dog_years}")
--- ExtraCredit/logarithm.py
+++ ExtraCredit/logarithm.py
@@ -8,6 +8,12 @@ Only use the log function from the math module.
 """
 from math import log
 
+
+
 if __name__ == "__main__":
-    ...
+    num1: float = log(10000) / log(10)
+    num2: float = log(1024) / log(2)
+    print(f"log of 10000 to the base 10: {num1}")
+    print(f"log of 1024 to the base 2: {num2}")
+    
     
