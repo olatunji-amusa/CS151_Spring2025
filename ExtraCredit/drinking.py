e67d02a1fab2c3feb3beb18132449429d28552b8
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-03-14 12:30:30 -0400

    Repository Update - 2025-03-14 12:30:29

--- ExtraCredit/drinking.py
+++ ExtraCredit/drinking.py
@@ -9,3 +9,12 @@ If the user does not input yes or no, display to the user they cannot drink.
 
 if __name__ == "__main__":
     ...
+    has_id = input("Do you have your ID? (yes or no): ").lower()
+    if has_id == "yes":
+        is_21 = input("Are you 21 years or older? (yes or no): ").lower()
+        if is_21 == "yes":
+            print("You can drink.")
+        else:
+            print("You cannot drink.")
+    else:
+        print("You cannot drink.")
