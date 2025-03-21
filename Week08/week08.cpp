c74d6f8bb4ee002975e808a3e3bb93236152c48b
jonathan-skeete <jonathan.skeete.dev@gmail.com>
2025-03-21 15:08:58 -0400

    Repository Update - 2025-03-21 15:08:57

--- Week08/week08.cpp
+++ Week08/week08.cpp
@@ -2,13 +2,13 @@
     Write a program that stores the word for each digit in an array.
     The program should ask the user to enter a number between 0 and 9 and
     should display the corresponding word. For example, if the user enters 4,
-    the program should display "four." Use a switch statement to determine the
-    word to display from the array. There should 10 cases [0,9]. If the number is not between 0 and 9, the program should 
+    the program should display "four." Use a switch statement to determine which
+    word from the array to display. There should 10 cases [0,9]. 
+    If the number is not between 0 and 9, the program should 
     display "Not a valid digit."
 
     Optimization: Can you get the same result without using a switch statement and without so many cases?
 */
-
 #include <iostream>
 #include <string>
 
