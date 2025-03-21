975079576d841a135e7d971c0fac800bffd8d2b4
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-03-21 14:44:03 -0400

    Repository Update - 2025-03-21 14:44:02

--- Week08/week08.cpp
+++ Week08/week08.cpp
@@ -3,10 +3,10 @@
     The program should ask the user to enter a number between 0 and 9 and
     should display the corresponding word. For example, if the user enters 4,
     the program should display "four." Use a switch statement to determine the
-    word to display from the array. If the number is not between 0 and 9, the program should 
-    display "Not a valid digit and exit. 
+    word to display from the array. There should 10 cases [0,9]. If the number is not between 0 and 9, the program should 
+    display "Not a valid digit."
 
-    Optimization: Can you get the same result without using a switch statement?
+    Optimization: Can you get the same result without using a switch statement and without so many cases?
 */
 
 #include <iostream>
@@ -16,6 +16,6 @@ using namespace std;
 
 int main()
 {
-    
+
     return 0;
 }
