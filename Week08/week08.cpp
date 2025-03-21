60e4486d665f61295a36a1716f9569b8022db80f
Jonathan-Skeete <Jonathan.Skeete.dev@gmail.com>
2025-03-21 14:32:58 -0400

    Repository Update - 2025-03-21 14:32:58

---
+++ Week08/week08.cpp
@@ -0,0 +1,21 @@
+/*
+    Write a program that stores the word for each digit in an array.
+    The program should ask the user to enter a number between 0 and 9 and
+    should display the corresponding word. For example, if the user enters 4,
+    the program should display "four." Use a switch statement to determine the
+    word to display from the array. If the number is not between 0 and 9, the program should 
+    display "Not a valid digit and exit. 
+
+    Optimization: Can you get the same result without using a switch statement?
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
