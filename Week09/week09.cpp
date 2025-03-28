46d6b74aadc37f5bae97cd445de80c01a22b7a45
jonathan-skeete <jonathan.skeete.dev@gmail.com>
2025-03-28 15:37:19 -0400

    Repository Update - 2025-03-28 15:37:18

--- Week09/week09.cpp
+++ Week09/week09.cpp
@@ -5,6 +5,32 @@ using namespace std;
 
 int main()
 {
+    /*
+    string name;
+
+    cout << "Please enter your name: ";
+    cin >> name;
+
+    while (name != "stop")
+    {
+        cout << "Hi " << name << endl;
+        cout << "Please enter your name: ";
+        cin >> name;
+    }
+    */
+
+    // int num = 60;
+    // do
+    // {
+    //     cout << num-- << endl;
+    // } while(num >= 0);
+
+    for (int num = 0; num * 5 <= 100; num++)
+    {
+        cout << num * 5 << endl;
+    }
     
+
+
     return 0;
 }
