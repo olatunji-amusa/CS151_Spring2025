9bea7b7f8c6ba27023d4b070eafa8bf9daf62807
jonathan-skeete <jonathan.skeete.dev@gmail.com>
2025-04-04 15:23:53 -0400

    Repository Update - 2025-04-04 15:23:53

---
+++ ExtraCredit/factorial.py
@@ -0,0 +1,14 @@
+'''
+    Write a program that calculates and displays the factorial of a user-defined integer.
+    The factorial of a number is the product of the number and the numbers prior up to 1.
+    For example 5 facorial (5!) is:
+    
+    5 * 4 * 3 * 2 * 1 = 120
+
+    Validate the user-defined integer. Ensure the user enters a positive number.
+    Note: 0! = 1
+'''
+
+if __name__ == "__main__":
+    ...
+    
